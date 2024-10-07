00a31f6bccdab386c4d464c8ad8d5aac3a2f140d
Vivek Kumar <vivekkumar.1644@gmail.com>
2024-10-07 17:10:49 +0530

    feat: implement load balanced api gateway

---
+++ src/main/java/com/example/ecommerceapigateway/EcommerceapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceapigatewayApplication.class, args);
+	}
+
+}
